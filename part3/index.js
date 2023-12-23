a613e893d7a2d003ef051199385b1631cacbf212
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2023-12-24 00:55:19 +0300

    Finished 3.5

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let phonebook = [
   {
     id: 1,
@@ -27,16 +29,18 @@ let phonebook = [
 app.get("/api/persons", (request, response) => {
   response.json(phonebook);
 });
+
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = phonebook.find(person => person.id === id);
-  
+  const person = phonebook.find((person) => person.id === id);
+
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
 });
+
 app.get("/info", (request, response) => {
   response.send(
     `<div><p>Phonebook has info for ${
@@ -44,6 +48,38 @@ app.get("/info", (request, response) => {
     } people</p><p>${new Date()}</p></div>`
   );
 });
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  phonebook = phonebook.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+function generateId() {
+  const maxId = Math.floor(Math.random() * 1000000);
+  return maxId;
+}
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const phone = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  phonebook = phonebook.concat(phone);
+
+  response.json(phone);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
